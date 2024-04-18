2b25ef501a0bce0df9b447c964459ebaeb491c27
viniboy2 <144129498+viniboy2@users.noreply.github.com>
2024-04-18 16:02:47 -0300

    ,

---
+++ Senha fixa 1114.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+   int i;
+   int j=0;
+      wile (j=0){
+   scanf("%d",i);
+    if (i=2002){
+        printf("Acesso Permitido");
+        j=1;
+            }
+   else{
+       printf("Senha Invalida");
+       j=0;
+   }
+      }
+    return 0;
+}
